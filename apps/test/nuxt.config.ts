93897cfbd731955c661b07bdf935abf4019fad83
Stephane P. Pericat <contact@pericat.net>
2025-10-06 10:25:17 -0500

    chore: test app

--- apps/test/nuxt.config.ts
+++ apps/test/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-});
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
+})
