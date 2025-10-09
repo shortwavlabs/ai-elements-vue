b9495cec742084a69eafca7b70343d74f1ea481c
Stephane P. Pericat <contact@pericat.net>
2025-10-09 11:42:49 -0500

    feat: canvas

---
+++ apps/test/app/pages/examples/canvas.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Canvas } from '@repo/elements/canvas'
+
+const nodes = ref([
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+  { id: '2', label: 'Node 2', position: { x: 100, y: 100 } },
+  { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
+  { id: '4', label: 'Node 4', position: { x: 400, y: 200 } },
+])
+
+const edges = ref([
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e1-3', source: '1', target: '3' },
+])
+</script>
+
+<template>
+  <div class="h-[480px] w-[640px] border rounded-md p-4">
+    <Canvas :edges="edges" :nodes="nodes" />
+  </div>
+</template>
---
+++ packages/elements/src/canvas/Canvas.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { VueFlow, type Edge, type Node } from '@vue-flow/core'
+
+import '@vue-flow/core/dist/style.css'
+import '@vue-flow/core/dist/theme-default.css'
+
+defineProps<{
+  edges: Edge[]
+  nodes: Node[]
+}>()
+</script>
+<template>
+  <VueFlow class="h-full w-full" :edges="edges" :nodes="nodes" />
+</template>
