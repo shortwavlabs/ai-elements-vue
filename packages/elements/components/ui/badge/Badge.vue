929c1518d869816315af762ccb743ce1b376997b
Stephane P. Pericat <contact@pericat.net>
2025-10-10 10:23:35 -0500

    fix: badge and button errors

--- packages/elements/components/ui/badge/Badge.vue
+++ packages/elements/components/ui/badge/Badge.vue
@@ -20,7 +20,7 @@ const delegatedProps = reactiveOmit(props, 'class')
 <template>
   <Primitive
     data-slot="badge"
-    :class="cn(badgeVariants({ variant }), props.class)"
+    :class="cn(badgeVariants({ variant: props.variant }), props.class)"
     v-bind="delegatedProps"
   >
     <slot />
--- packages/elements/components/ui/button/Button.vue
+++ packages/elements/components/ui/button/Button.vue
@@ -20,9 +20,14 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <Primitive
     data-slot="button"
-    :as="as"
-    :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :as="props.as"
+    :as-child="props.asChild"
+    :class="
+      cn(
+        buttonVariants({ variant: props.variant, size: props.size }),
+        props.class,
+      )
+    "
   >
     <slot />
   </Primitive>
