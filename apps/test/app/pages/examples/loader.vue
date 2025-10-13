1dd38aca76cc4e84bdd0bda227f71f5ef2d5b201
Stephane P. Pericat <contact@pericat.net>
2025-10-13 17:10:08 -0500

    ferat: loader component

---
+++ apps/test/app/pages/examples/loader.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Loader } from '@repo/elements/loader'
+</script>
+<template>
+  <div className="flex items-center justify-center p-8">
+    <Loader />
+  </div>
+</template>
